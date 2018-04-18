04de63137f3d7a60b5c2feed9db8b651c7ec2fc8
dqgorelick <dqgorelick@gmail.com>
2018-04-18 09:03:36 -0400

    refactors parsing

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,9 @@
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-var MULTIPLE_STRING = 'that link has been submitted to multiple subreddits. you can try to'
-var SINGLE_STRING = 'link has already been submitted, but you can try to'
-
-var SCORE_THRESHOLD = 5
+const MULTIPLE_STRING = 'that link has been submitted to multiple subreddits. you can try to'
+const SINGLE_STRING = 'link has already been submitted, but you can try to'
+const RESULT_MULTIPLE = 'multiple'
+const RESULT_SINGLE = 'single'
+const RESULT_NONE = 'none'
+const SCORE_THRESHOLD = 25
 
 var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
@@ -20,113 +18,130 @@ var HttpClient = function() {
     }
 }
 
-//example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//   	chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   });
-
-// function parseArticle(response) {
-
-// }
+function parseArticle(article, cb) {
+  var result = {}
+  var html = $(article)
+  var votes = html.find('.score.unvoted')
+  if (votes.length > 0) {
+    result.score = votes[0].innerText
+    if (parseInt(result.score) < SCORE_THRESHOLD) {
+      return cb(false)
+    }
+  }
+  var title = html.find('p.title')
+  if (title.length > 0) {
+    result.title = title[0].innerText
+  }
+  // article list
+  var subreddit = html.find('a.subreddit')
+  if (subreddit.length > 0) {
+    result.subreddit = subreddit[0].innerText
+    result.href = subreddit[0].href
+  }
+  // single page
+  subreddit = html.find('.pagename.redditname a')
+  if (subreddit.length > 0) {
+    result.subreddit = subreddit[0].innerText
+    result.href = subreddit[0].href
+  }
+  var comments = html.find('.flat-list.buttons .first a')
+  if (comments.length > 0) {
+    result.comments = comments[0].href
+  }
+  if (result.title) {
+    return cb(result)
+  }
+  return cb(false)
+}
 
 function parseMultipleArticles(response) {
   var results = []
   var articles = $(response).find('#siteTable').children()
   for (var i=0; i < articles.length; i++) {
-    var result = {}
-    var votes = $(articles[i]).find('.score.unvoted')
-    if (votes.length > 0) {
-      result.score = votes[0].innerText
-      if (parseInt(result.score) < SCORE_THRESHOLD) {
-        continue
+    var result = parseArticle(articles[i], (result) => {
+      if (result) {
+        results.push(result)
+      }
+    })
+    if (i === articles.length-1) {
+      if (results.length) {
+        return {
+          result: RESULT_MULTIPLE,
+          articles: results
+        }
+      }
+      return {
+        result: RESULT_NONE
       }
     }
-    var title = $(articles[i]).find('p.title')
-    if (title.length > 0) {
-      result.title = title[0].innerText
-    } else {
-      continue
-    }
-    var subreddit = $(articles[i]).find('a.subreddit')
-    if (subreddit.length > 0) {
-      result.subreddit = subreddit[0].innerText
-      result.href = subreddit[0].href
-    }
-    results.push(result)
   }
-  return results
 }
+
 
 function parseSingleArticle(response) {
-  var html = $(response)
-  var votes = html.find('#siteTable .score.unvoted')[0].title
-  if (parseInt(votes) < SCORE_THRESHOLD) {
-    return false
-  }
-  var subreddit = html.find('.pagename a')[0]
-  var title = html.find('p.title')[0].innerText
-  return {
-    title: title,
-    subreddit: subreddit.innerText,
-    href: subreddit.href,
-    score: votes
-  }
+  var article = parseArticle(response, (article) => {
+    return article
+  })
 }
 
-chrome.extension.onConnect.addListener((port) => {
-  port.onMessage.addListener((msg) => {
-    var client = new HttpClient();
-    var url = 'https://www.reddit.com/submit?url=' + msg
-    client.get(url, function(response) {
-      if (response.includes(MULTIPLE_STRING)) {
-        var results = parseMultipleArticles(response)
-        console.log("multiple", results)
-        port.postMessage(JSON.stringify(results))
-      } else if (response.includes(SINGLE_STRING)) {
-        var result = parseSingleArticle(response)
-        console.log('single', result)
-        port.postMessage(JSON.stringify(result))
-      } else {
-        port.postMessage(JSON.stringify({}));
-      }
-    });
-  })
-})
+// chrome.extension.onConnect.addListener((port) => {
+//   port.onMessage.addListener((msg) => {
+//     var client = new HttpClient();
+//     var url = 'https://www.reddit.com/submit?url=' + msg
+//     client.get(url, function(response) {
+//       if (response.includes(MULTIPLE_STRING)) {
+//         var parsed = parseMultipleArticles(response)
+//         if (parsed.result !== RESULT_NONE) {
+//           console.log("multiple", parsed.articles)
+//         }
+//         port.postMessage(JSON.stringify(parsed))
+//       } else if (response.includes(SINGLE_STRING)) {
+//         console.log(msg)
+//         var parsed = parseSingleArticle(response)
+//         port.postMessage(JSON.stringify(parsed))
+//       } else {
+//         port.postMessage(JSON.stringify({
+//           result: RESULT_NONE
+//         }));
+//       }
+//     });
+//   })
+// })
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (!request) {
+    if (!request)  {
       sendResponse(JSON.stringify({}));
     }
     var client = new HttpClient();
     var url = 'https://www.reddit.com/submit?url=' + request
     client.get(url, function(response) {
       if (response.includes(MULTIPLE_STRING)) {
-        var results = parseMultipleArticles(response)
-        if (results.length === 0) {
-          sendResponse(JSON.stringify({}))
-          return
+        var parsed = parseMultipleArticles(response)
+
+        if (parsed.result !== RESULT_NONE) {
+          console.log("multiple", parsed.articles)
         }
-        console.log("multiple: ", request, results)
-        sendResponse(JSON.stringify(results))
+        sendResponse(JSON.stringify(parsed))
       } else if (response.includes(SINGLE_STRING)) {
-        var result = parseSingleArticle(response)
-        if (result === false) {
-          sendResponse(JSON.stringify({}))
-          return
-        }
-        console.log('single', request, result)
-        sendResponse(JSON.stringify(result))
+        parseArticle(response, (article) => {
+          if (article) {
+            console.log('SINGLE parsed', article);
+            sendResponse(JSON.stringify({
+              result: RESULT_SINGLE,
+              article: article
+            }));
+          } else {
+            sendResponse(JSON.stringify({
+              result: RESULT_NONE
+            }));
+          }
+        })
       } else {
-        // console.log('none')
-        sendResponse(JSON.stringify({}));
+        sendResponse(JSON.stringify({
+          result: RESULT_NONE
+        }));
       }
     });
-
-    // if (request.greeting == "hello") {
-    //   sendResponse({farewell: "goodbye"});
-    // }
   });
